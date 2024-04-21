880758c363a8e3b306bbeace95142f2341d5eecb
asif986 <69723211+asif986@users.noreply.github.com>
2024-04-21 20:55:10 +0530

    Update surveyStore.js

--- models/surveyStore.js
+++ models/surveyStore.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const surveStoreSchema = mongoose.Schema({
     // required: true 
     surveyData: {type : String,},
+    successIndicatorScore:{type : String},
+    images:{type:String },
     project_id: {  type: mongoose.Schema.Types.ObjectId, ref: "Project", required: true },
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+
 });
 
 module.exports = mongoose.model('SurveyStore', surveStoreSchema);
