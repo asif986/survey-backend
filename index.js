ff34ee529e6adbc830cbbb69451911e284c50dbf
asif986 <69723211+asif986@users.noreply.github.com>
2024-04-27 11:20:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 
- app.use(bodyParser.json())
+// app.use(bodyParser.json())
 //  app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.urlencoded({ extended: false }));
 
 //process.env.MONGO_ATLAS_PS
 
