84c79a3b1822040726cbecb3e82b3e2d16776d6b
asif986 <69723211+asif986@users.noreply.github.com>
2024-05-01 11:58:54 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -8,4 +8,7 @@ router.post('/signup', UserController.createNewUser);
 
 router.post('/login', UserController.loin)
 
+router.get('/:id', UserController.getUserInformation)
+
+
 module.exports = router;
