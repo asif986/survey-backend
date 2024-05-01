6daa15a9134d18bcc459668437ab3b314a213cc4
asif986 <69723211+asif986@users.noreply.github.com>
2024-05-01 12:04:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const errorHandle = require('./middleware/error-handling');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+// app.use(express.json({limit: '50mb'}));
+// app.use(express.urlencoded({limit: '50mb'}));
 
 //process.env.MONGO_ATLAS_PS
 
@@ -30,6 +30,10 @@ mongoose
         console.log(e);
         console.log("Connection failed!" + e);
     });
+
+ app.use(bodyParser.json({ limit: "50mb" }))
+ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
+
 
 app.use("/images", express.static(path.join("backend/images")));
 app.use((req, res, next) => {
